5c5bfd5c70fc624d336ab3f415a628116c5e7643
SPDUK <spdevuk@gmail.com>
2018-10-02 21:15:06 +0100

    color for image

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -5,17 +5,18 @@ defmodule Identicon do
     |> pick_color
   end
 
-  def pick_color(image) do
-    # get the first 3 values and throw away the rest from Identicon.Image {hex: [55, 55, 55, ....etc]}
-    %Identicon.Image{hex: [r, g, b | _tail]} = image
-    [r, g, b]
-  end
-
   def hash_input(input) do
     hex =
       :crypto.hash(:md5, input)
       |> :binary.bin_to_list()
 
     %Identicon.Image{hex: hex}
+  end
+
+  # get the first 3 values and throw away the rest from Identicon.Image {hex: [55, 55, 55, ....etc]}
+  # pattern matching r,g,b out of image while recieving image as an argument at the same time
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    # add color key/value pair to the struct with pipe, adding in rgb
+    %Identicon.Image{image | color: {r, g, b}}
   end
 end
