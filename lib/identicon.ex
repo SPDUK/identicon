55a10b073dc565fd2ef3e94da57c5a4168e04d74
SPDUK <spdevuk@gmail.com>
2018-10-02 20:55:56 +0100

    pick_color & hash_input functions

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,18 +1,21 @@
 defmodule Identicon do
-  @moduledoc """
-  Documentation for Identicon.
-  """
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_color
+  end
 
-  @doc """
-  Hello world.
+  def pick_color(image) do
+    # get the first 3 values and throw away the rest from Identicon.Image {hex: [55, 55, 55, ....etc]}
+    %Identicon.Image{hex: [r, g, b | _tail]} = image
+    [r, g, b]
+  end
 
-  ## Examples
+  def hash_input(input) do
+    hex =
+      :crypto.hash(:md5, input)
+      |> :binary.bin_to_list()
 
-      iex> Identicon.hello()
-      :world
-
-  """
-  def hello do
-    :world
+    %Identicon.Image{hex: hex}
   end
 end
