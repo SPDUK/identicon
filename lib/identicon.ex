6b66b52d27dd431f192e8e585cc37bb9534bf90f
SPDUK <spdevuk@gmail.com>
2018-10-03 13:08:35 +0100

    build_grid and mirror_row

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -6,17 +6,18 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
   end
 
   @doc """
   Takes a string `input` and hashes it, turning it to a list afterwards.
   returns `Identicon.Image` updated with the hashed list as the hex value
   ## Examples
-    iex> Identicon.hash_input("hello")
-    %Identicon.Image{
-    color: nil,
-    hex: [93, 65, 64, 42, 188, 75, 42, 118, 185, 113, 157, 145, 16, 23, 197, 146]
-    }
+      iex> Identicon.hash_input("hello")
+      %Identicon.Image{
+      color: nil,
+      hex: [93, 65, 64, 42, 188, 75, 42, 118, 185, 113, 157, 145, 16, 23, 197, 146]
+      }
   """
   @spec hash_input(String.t()) :: Identicon.Image.t()
   def hash_input(input) do
@@ -40,5 +41,27 @@ defmodule Identicon do
   """
   def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
     %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    grid =
+      hex
+      |> Enum.chunk(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten()
+      |> Enum.with_index()
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  @doc """
+    Takes a list of 3 elements and returns a list of 5,  mirrored.
+    ## Examples
+      iex> Identicon.mirror_row([145,46,200])
+        [145, 46, 200, 46, 145]
+  """
+  def mirror_row(row) do
+    [first, second | _tail] = row
+    row ++ [second, first]
   end
 end
